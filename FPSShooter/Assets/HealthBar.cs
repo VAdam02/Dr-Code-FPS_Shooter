d8b9e642b06e8c6c595f9cfecdcb0d52c3046b54
VAdam <adam.vilanyi2.program@gmail.com>
2021-06-10 15:52:46 +0200

    2021.04.30

---
+++ FPSShooter/Assets/HealthBar.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public GameObject target;
+    public Camera cam;
+    public double value = 0;
+
+    int max = 73;
+    GameObject bar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bar = gameObject.transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //jetpack jetpack = (jetpack)jetpackObject.GetComponent(typeof(jetpack));
+
+        RectTransform rect = (RectTransform)bar.GetComponent(typeof(RectTransform));
+        int size = (int)(max * value);
+        int pos = (max - size) / -2;
+        rect.sizeDelta = new Vector2(size, 7);
+        rect.localPosition = new Vector3(pos, 0, 0);
+
+        Vector3 screenpoint = cam.WorldToScreenPoint(target.transform.position + new Vector3(0, 1, 0));
+        RectTransform rect2 = (RectTransform)gameObject.GetComponent(typeof(RectTransform));
+        rect2.position = screenpoint;
+    }
+}
--- FPSShooter/Assets/jerrycan.cs
+++ FPSShooter/Assets/jerrycan.cs
@@ -4,28 +4,58 @@ using UnityEngine;
 
 public class jerrycan : MonoBehaviour
 {
+    public double deltatime = 0;
+    GameObject can;
+
     // Start is called before the first frame update
     void Start()
     {
+        can = gameObject.transform.GetChild(0).gameObject;
         gameObject.SetActive(true);
     }
 
+    float position = 0;
     // Update is called once per frame
     void Update()
     {
-        
+        if (deltatime > 10)
+        {
+            can.SetActive(true);
+        }
+        else
+        {
+            deltatime += Time.deltaTime;
+        }
+
+        can.transform.localEulerAngles = new Vector3(can.transform.eulerAngles.x, can.transform.eulerAngles.y + (360 / 6 * Time.deltaTime), can.transform.eulerAngles.z);
+
+        position += Time.deltaTime / 2;
+        if (position < 1)
+        {
+            can.transform.localPosition = new Vector3(can.transform.localPosition.x, position, can.transform.localPosition.z);
+        }
+        else if (position < 2)
+        {
+            can.transform.localPosition = new Vector3(can.transform.localPosition.x, 2 - position, can.transform.localPosition.z);
+        }
+        else
+        {
+            position -= 2;
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("igen");
-        jetpack jetpack = (jetpack) collision.gameObject.GetComponent(typeof(jetpack));
+        GameObject jetpackObject = collision.gameObject.transform.GetChild(2).gameObject;
+
+        jetpack jetpack = (jetpack)jetpackObject.GetComponent(typeof(jetpack));
         jetpack.fuel = jetpack.fuel + 500;
         if (jetpack.fuel > 1000)
         {
             jetpack.fuel = 1000;
         }
-        
+        deltatime = 0;
+
         gameObject.transform.GetChild(0).gameObject.SetActive(false);
     }
 }
