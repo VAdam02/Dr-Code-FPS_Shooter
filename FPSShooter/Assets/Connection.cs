af2df949660ef1c8492b3e7df788a2f00ab3ec40
VAdam <adam.vilanyi2.program@gmail.com>
2021-06-10 15:24:47 +0200

    2021.02.12

--- FPSShooter/Assets/Connection.cs
+++ FPSShooter/Assets/Connection.cs
@@ -18,6 +18,58 @@ public class Connection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        stream.Write(new byte[] { 0, 1, 2, 3, 4, 5, 6 }, 0, 7);
+
+    }
+
+    public static void WriteData(byte[] data)
+    {
+        stream.Write(data, 0, data.Length);
+    }
+    public static void WriteData(Vector3 pos, Vector3 rot)
+    {
+        byte[] data = new byte[7];
+
+
+    }
+
+    public static byte DecToBin(int num)
+    {
+        int[] binary = new int[8];
+
+        double cache = num;
+        // +/-
+        if (cache < 0)
+        {
+            binary[0] = 0;
+            cache *= -1;
+        }
+        else
+        {
+            binary[0] = 1;
+        }
+
+        //dec to bin
+        int i = 7;
+        while (i > 0)
+        {
+            binary[i] = (int)(cache % 2);
+            cache = (int)(cache / 2);
+            i--;
+        }
+
+        Debug.Log(binary[0] + " " + binary[1] + " " + binary[2] + " " + binary[3] + " " + binary[4] + " " + binary[5] + " " + binary[6] + " " + binary[7]);
+
+        //bin to byte
+        byte data;
+        cache = 0;
+        i = 0;
+        while (i < 8)
+        {
+            cache *= 2;
+            cache += binary[i];
+            i++;
+        }
+        data = (byte)cache;
+        return data;
     }
 }
---
+++ FPSShooter/Assets/DEBUGSCRIPT.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEBUGSCRIPT : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int x = (int)gameObject.transform.position.x;
+        Debug.Log("x: " + x + "\t" + Connection.DecToBin(x));
+    }
+}
